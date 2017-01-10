a2f75841e0578901f8a9f7cd0692df66006d0dce
Gabriel Garcez Barros Sousa <gabrielgarcezbsousa@gmail.com>
2017-01-10 18:07:14 -0200

    Versão final do código

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-# coding=utf-8
 import random
+
+#Equipe: Anderson Matheus Passos Paiva, Dalai dos Santos Ribeiro, 
+#Gabriel Garcez Barros Sousa, Hevelys Sandes Oliveira e Polyana Bezerra da Costa.
+
 
 #formula da curva eliptica
 #y**2 = x**3 + a*x + b (mod p)
 a = 0
 b = 7
-p = 3
+p = 1461501637330902918203686915170869725397159163571
 E = [a,b,p]
 #parametros publicos da encriptacao
 #a curva definida acima
 #um ponto P da curva onde P e o gerador
 #a ordem n do subgrupo de E
-n = 1461501637330902918203686915170869725397159163571
+#n = 1461501637330902918203686915170869725397159163571
 P = (338530205676502674729549372677647997389429898939, 842365456698940303598009444920994870805149798382)
 G = (218446380845822009133260914022597958688441204967, 1014310923682614467817833035434561095879856320025)
 
 random.seed()
 
-def somaEliptica(pontoP, pontoQ, p):
+def somaEliptica(pontoP, pontoQ, E):
 	x1, y1 = pontoP
 	x2, y2 = pontoQ
 
@@ -31,19 +34,21 @@ def somaEliptica(pontoP, pontoQ, p):
 		if(y1 != y2):
 			return (float("inf"), float("inf"))
 
-	delta = (y2-y1)/(x2-x1)
-	x3 = pow((pow(delta, 2) - x1 - x2), 1, p)
-	y3 = pow((delta*(x1 - x3) - y1), 1, p)
-
+	delta = ((y2 - y1)*(pow(x2 - x1,  E[2] - 2, E[2])))%E[2]
+	#(y2-y1)/(x2-x1)
+	
+	x3 = pow((pow(delta, 2) - x1 - x2), 1, E[2])
+	y3 = pow((delta*(x1 - x3) - y1), 1, E[2])
+	
 	return (x3, y3)
 
-def duplicacaoEliptica(pontoP, p, a):
+def duplicacaoEliptica(pontoP, E):
 
 	if(pontoP == (float("inf"), float("inf"))):
 		return pontoP
 
 	x1, y1 = pontoP
-	delta = (3*pow(x1,2) + a)/(2*y1)
+	delta = ((3*(pow(x1, 2)) + E[0])*(pow(2*y1, E[2] - 2, E[2])))%E[2]
 	x3 = pow((pow(delta, 2) - 2*x1), 1, p)
 	y3 = pow((delta*(x1 - x3) - y1), 1, p)
 
@@ -55,33 +60,38 @@ def intTobinlist(k):
 def inverterPonto(pontoP):
 	return pontoP[0], -pontoP[1]
 
-def multEscalarEliptica(pontoP, k):
+def multEscalarEliptica(pontoP, k, E):
 	binK = intTobinlist(k)
 	pontoQ = pontoP
 	for i in reversed(binK):
-		pontoQ = duplicacaoEliptica(pontoQ, p, a)
+		pontoQ = duplicacaoEliptica(pontoQ, E)
 		if i == 1:
-			pontoQ = somaEliptica(pontoQ, pontoP, p)
+			pontoQ = somaEliptica(pontoQ, pontoP, E)
 	return pontoQ
 
-def geraChave(E, P, n):
-	d = random.randint(1,n-1)
-	Q = multEscalarEliptica(P,d)
+def geraChave(E, P):
+	d = random.randint(1,E[2])
+	Q = multEscalarEliptica(P,d,E)
 	return (Q,d)
 
-def encryption(E, P, n, Q, M):
-	k = random.randint(1, n-1)
-	C1 = multEscalarEliptica(P, k)
-	C2 = somaEliptica(M, multEscalarEliptica(Q, k), E[2])
+def encryption(E, P, Q, M):
+	k = random.randint(1, E[2])
+	C1 = multEscalarEliptica(P, k, E)
+	C2 = somaEliptica(M, multEscalarEliptica(Q, k, E), E)
 	return (C1,C2)
 
 def decryption(C1, C2, d, E):
-	M = somaEliptica(C1, inverterPonto(multEscalarEliptica(C2,d)), E[2])
+	M = somaEliptica(C2, inverterPonto(multEscalarEliptica(C1,d, E)), E)
 	return M
 
-Q, d = geraChave(E, P, n)
-print Q, d
-C1, C2 = encryption(E, P, n, Q, G)
-print C1, C2
+print "Ponto original (mensagem): ", G
+Q, d = geraChave(E, P)
+print "\nPar de chaves \nPública (Q) = ", Q ,"\nPrivada (d) = ", d
+C1, C2 = encryption(E, P, Q, G)
+print "\nEncriptação: \nC1 = ", C1,"\nC2 = ", C2
 M = decryption(C1, C2, d, E)
-print M
+print "\nDecriptação (mensagem): ", M
+if(G == M):
+ print "Mensagem decriptada!"
+else:
+  print "Mensagem não decriptada."
